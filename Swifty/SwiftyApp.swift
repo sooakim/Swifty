4a4988d9a323e553829b53fb6f15ded66faf1b89
김수아 <sooakim@gimsuas-Mac-Studio.local>
2023-07-28 23:46:21 +0900

    initial commit

---
+++ Swifty/SwiftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyApp.swift
+//  Swifty
+//
+//  Created by 김수아 on 2023/07/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
